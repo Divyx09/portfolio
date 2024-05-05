409f735a24ec8506917632420cf514bd2c3723e5
Divyx09 <divyaattarde94@gmail.com>
2024-05-05 07:14:54 +0530

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <div className="nav-item-left">Divya Attarde</div>
+          <div className="nav-item-right">
+            <ul>
+              <li> Home </li>
+              <li> About </li>
+              <li> Services </li>
+              <li> Projects </li>
+              <li> Certificates </li>
+              <li> Contact </li>
+            </ul>
+          </div>
+        </nav>
+      </header>
+    </div>
+  );
+}
